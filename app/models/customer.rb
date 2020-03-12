2ef8d477ad3c7ea5d4b69b378052b11d93a6b520
jlee <yeoniee217@gmail.com>
2020-03-12 11:40:48 -0500

    Added root route and validations to Customer model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
   validates :full_name, presence: true
+  validates :notes, length: { maximum: 300 }
+  validates :email, uniqueness: true
 
   has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  root to: "customers#index"
   resources "customers", only: %i[index]
   get "/customers/alphabetized", to: "customers#alphabetized"
   get "/customers/missing_email", to: "customers#missing_email"
