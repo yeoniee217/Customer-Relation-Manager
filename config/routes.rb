ac2dfd41c3c068a977715e40de3ad9fdb34e315e
jlee <yeoniee217@gmail.com>
2020-03-12 10:30:30 -0500

    Created customers controller and added actions and views for them.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources "customers", only: %i[index]
+  get "/customers/alphabetized", to: "customers#alphabetized"
+  get "/customers/missing_email", to: "customers#missing_email"
 end
